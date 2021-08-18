ed528857aa2785b06a7899e797321edc6ad2dc33
yash gupta <yash3144@gmail.com>
2021-08-18 15:27:23 +0530

    Add logger property for logging messages

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    // {
+    //   logger: ['error', 'warn', 'debug']
+    // }  
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
