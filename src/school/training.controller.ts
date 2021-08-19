f76bfae058c1c87149b1b37a75b37fb7c756f420
yash gupta <yash3144@gmail.com>
2021-08-19 20:46:13 +0530

    api in subject controller

--- src/school/training.controller.ts
+++ src/school/training.controller.ts
@@ -15,19 +15,20 @@ export class TrainingController {
 
   @Post('/create')
   public async savingRelation() {
-    // const subject = new Subject();
-    // subject.name = 'Math';
+    const subject = new Subject();
+    subject.name = 'Math';
 
-    const subject = await this.subjectRepository.findOne(3);
+    // const subject = await this.subjectRepository.findOne(3);
 
-    // const teacher1 = new Teacher();
-    // teacher1.name = 'John Doe';
+    const teacherOne = new Teacher();
+    teacherOne.name = 'John Doe';
 
-    // const teacher2 = new Teacher();
-    // teacher2.name = 'Harry Doe';
+    const teacherTwo = new Teacher();
+    teacherTwo.name = 'Harry Doe';
 
-    // subject.teachers = [teacher1, teacher2];
-    // await this.teacherRepository.save([teacher1, teacher2]);
+    subject.teachers = [teacherOne, teacherTwo];
+    await this.teacherRepository.save([teacherOne, teacherTwo]);
+    await this.subjectRepository.save(subject);
 
     // How to use One to One
     // const user = new User();
@@ -38,31 +39,31 @@ export class TrainingController {
     // Save the user here
 
 
-    const teacher1 = await this.teacherRepository.findOne(5);
-    const teacher2 = await this.teacherRepository.findOne(6);
+    // const teacher1 = await this.teacherRepository.findOne(5);
+    // const teacher2 = await this.teacherRepository.findOne(6);
 
-    return await this.subjectRepository
-      .createQueryBuilder()
-      .relation(Subject, 'teachers')
-      .of(subject)
-      .add([teacher1, teacher2]);
+    // return await this.subjectRepository
+    //   .createQueryBuilder()
+    //   .relation(Subject, 'teachers')
+    //   .of(subject)
+    //   .add([teacher1, teacher2]);
   }
 
   @Post('/remove')
   public async removingRelation() {
-    // const subject = await this.subjectRepository.findOne(
-    //   1,
-    //   { relations: ['teachers'] }
-    // );
+    const subject = await this.subjectRepository.findOne(
+      1,
+      { relations: ['teachers'] }
+    );
 
-    // subject.teachers = subject.teachers.filter(
-    //   teacher => teacher.id !== 2
-    // );
+    subject.teachers = subject.teachers.filter(
+      teacher => teacher.id !== 2
+    );
 
-    // await this.subjectRepository.save(subject);
-    await this.subjectRepository.createQueryBuilder('s')
-      .update()
-      .set({ name: "Confidential" })
-      .execute();
+    await this.subjectRepository.save(subject);
+    // await this.subjectRepository.createQueryBuilder('s')
+    //   .update()
+    //   .set({ name: "Confidential" })
+    //   .execute();
   }
 }
